172077bbf48c1432d13b6d1ccf8631b5b692c0d8
Weilong Li <zickyls@qq.com>
2017-12-05 12:33:31 +0100

    Update Parameter_space.R

--- Parameter_space.R
+++ Parameter_space.R
@@ -5,18 +5,18 @@ parameter.space <- NULL
 parameter.space$twinv <- c(0, 1)
 
 ## Proportion to be discards ====
-parameter.space$dinPv <- c(0.15, 0.1, 0.05, 0)
+parameter.space$dinPv <- c(0.4, 0.3, 0.2, 0.1, 0.05, 0)
 
 ## Methylation ====
 # M-value population mean
-parameter.space$methylation$meanMv <- c(-3.78, 0, 2)
+parameter.space$methylation$meanMv <- c(-3.78, 3.36)
 # methylation scale
-parameter.space$methylation$rsqEnv <- c(0.1, 0.5, 1)
+parameter.space$methylation$rsqEnv <- c(0.1, 0.3, 0.5, 0.8, 1)
 # M-value variace of case and control group
-parameter.space$methylation$varMv <- c(0.5, 1, 1.5, 2)
+parameter.space$methylation$varMv <- c(0.06, 1, 2, 3, 4)
 # methylation error twin correlation
-parameter.space$methylation$corMEv <- c(0, 0.5)
+parameter.space$methylation$corMEv <- c(0, 0.1, 0.3, 0.5, 0.8)
 
 ## regression ====
 # genotype
-parameter.space$heritabilityv <- c(0.5, 0.4, 0.3, 0.2, 0.1, 0)
+parameter.space$heritabilityv <- c(0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0)
